0face5baa93840da7bc476c0a0d5de3a775343fc
NathanThus <nathan.thus@outlook.com>
2024-03-28 12:06:58 +0100

    🚧 Work in Progress
    Shelving this project for a bit, as I'm just getting stuck and frustrated.

--- Assets/Scripts/TextMeshAnimator.cs
+++ Assets/Scripts/TextMeshAnimator.cs
@@ -3,37 +3,61 @@ using TMPro;
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 [RequireComponent(typeof(TMP_Text))]
 public class TextMeshAnimator : MonoBehaviour
 {
-    [SerializeField] protected float _horizontalWobble = 3.3f;
-    [SerializeField] protected float _verticalWobble = 0.8f;
-    [SerializeField] protected Gradient _rainbow;
-    [SerializeField] protected List<int> _words;
-    protected TMP_Text _textMesh;
-    protected Mesh _mesh;
-    protected Vector3[] _verticies;
-    protected List<int> _wordLengths = new();
-    protected List<int> _wordIndexes= new(){0};
-    protected Color[] _colors;
+    [SerializeField] private float _horizontalWobble = 3.3f;
+    [SerializeField] private float _verticalWobble = 0.8f;
+    [SerializeField] private string _indicator;
+    [SerializeField] private Gradient _rainbow;
+    [SerializeField] private List<string> _texts;
 
-    private string _regex = "<R>";//"<[a-zA-Z]+>";
-    RegexOptions _regexOptions = RegexOptions.Multiline;
+    private TMP_Text _textMesh;
+    private Mesh _mesh;
+    private Vector3[] _verticies;
+    private List<int> _wordIndexes = new() { 0 };
+    private Color[] _colors;
 
+    private string _regex;
+    private string _enhancementIndicatorStart;
+    private string _enhancementIndicatorEnd;
     const int VerticeCount = 4;
-    // Start is called before the first frame update
+
+
     private void Start()
     {
         _textMesh = GetComponent<TMP_Text>();
-        PrepareWords();
+        _enhancementIndicatorStart = "<" + _indicator + ">";
+        _enhancementIndicatorEnd = "</" + _indicator + ">";
+        _regex = _enhancementIndicatorStart + "[a-zA-Z ]*</" + _enhancementIndicatorEnd;
+        PrepareText();
+    }
+
+    public void PrepareText()
+    {
+        _texts = _textMesh.text.Split(_enhancementIndicatorEnd).ToList<string>();
+        List<string> textToRemove = new();
+        foreach (var text in _texts)
+        {
+            if(!text.Contains(_enhancementIndicatorStart))
+             {
+                textToRemove.Add(text);   
+             }
+        }
+
+        foreach (var text in textToRemove)
+        {
+            _texts.Remove(text);
+        }
+
+        _textMesh.text = _textMesh.text.Replace(_enhancementIndicatorEnd, "");
+        _textMesh.text = _textMesh.text.Replace(_enhancementIndicatorStart, "");
     }
 
     private void Update()
     {
-        GatherData();
-        VerticePerWord();
-        ColorPerWord();
         SetMeshProperties();
     }
 
@@ -42,31 +66,6 @@ public class TextMeshAnimator : MonoBehaviour
         _textMesh.ForceMeshUpdate();
         _mesh = _textMesh.mesh;
         _verticies = _mesh.vertices;
-    }
-
-    /// <summary>
-    /// Prepares the system by separating the words by spacing.
-    /// </summary>
-    protected void PrepareWords()
-    {
-        string text = _textMesh.text;
-        for (int i = text.IndexOf(' '); i > -1; i = text.IndexOf(' ', i + 1))
-        {
-            _wordLengths.Add(i - _wordIndexes[_wordIndexes.Count -1]);
-            _wordIndexes.Add(i + 1);
-        }
-
-        _wordLengths.Add(text.Length - _wordIndexes[_wordIndexes.Count - 1]);
-
-        for (int i = 0; i < _wordIndexes.Count; i++)
-        {
-            string testString = text.Substring(_wordIndexes[i], _wordLengths[i]);
-            if(Regex.IsMatch(testString, _regex))
-            {
-                _words.Add(i);
-            }
-                
-        }
     }
 
     protected void VerticePerLetter()
@@ -84,66 +83,30 @@ public class TextMeshAnimator : MonoBehaviour
         }
     }
 
-    protected void VerticePerWord()
-    {
-        for (int w = 0; w < _wordIndexes.Count; w++)
-        {
-            if(!_words.Contains(w))
-            {
-                continue;
-            }
-
-            int word = _wordIndexes[w];
-            Vector3 offset = Wobble(Time.time + w);
-
-            for (var i = 0; i < _wordLengths[w]; i++)
-            {
-                TMP_CharacterInfo characterInfo = _textMesh.textInfo.characterInfo[word + i];
-
-                int vertexIndex = characterInfo.vertexIndex;
-                
-                for (int j = 0; j < VerticeCount; j++)
-                {
-                    _verticies[vertexIndex + j] += offset;
-                }
-            }
-        }
-    }
-
     protected void ColorPerWord()
     {
         _colors = _mesh.colors;
 
-        for (int w = 0; w < _wordIndexes.Count; w++)
-        {
-            if(!_words.Contains(w))
-            {
-                continue;
-            }
+        // for (int w = 0; w < _wordIndexes.Count; w++)
+        // {
+        //     // if (!_texts.Contains(w))
+        //     // {
+        //     //     continue;
+        //     // }
 
-            int word = _wordIndexes[w];
+        //     int word = _wordIndexes[w];
 
-            for (var i = 0; i < _wordLengths[w]; i++)
-            {
-                TMP_CharacterInfo characterInfo = _textMesh.textInfo.characterInfo[word + i];
-                int index = characterInfo.vertexIndex;
+        //     for (var i = 0; i < _wordLengths[w]; i++)
+        //     {
+        //         TMP_CharacterInfo characterInfo = _textMesh.textInfo.characterInfo[word + i];
+        //         int index = characterInfo.vertexIndex;
 
-                for (int j = 0; j < 4; j++)
-                {
-                    _colors[index + j] = _rainbow.Evaluate(Mathf.Repeat(Time.time + _verticies[index + j].x*0.001f, 1f));
-                }
-            }
-        }
-    }
-
-    protected void VerticePerMesh()
-    {
-        for (var i = 0; i < _verticies.Length; i++)
-        {
-            Vector3 offset = Wobble(Time.time + i);
-            _verticies[i] += offset;
-        }
-
+        //         for (int j = 0; j < 4; j++)
+        //         {
+        //             _colors[index + j] = _rainbow.Evaluate(Mathf.Repeat(Time.time + _verticies[index + j].x * 0.001f, 1f));
+        //         }
+        //     }
+        // }
     }
 
     /// <summary>
@@ -152,6 +115,8 @@ public class TextMeshAnimator : MonoBehaviour
 
     protected void SetMeshProperties()
     {
+        if(_mesh == null) return;
+
         _mesh.colors = _colors;
         _mesh.vertices = _verticies;
         _textMesh.canvasRenderer.SetMesh(_mesh);
