112f4034c515100fd0a3b3548e11150921624ba0
NathanThus <nathan.thus@outlook.com>
2024-05-09 12:12:34 +0200

    ✨ Create animation Template

---
+++ Assets/Scripts/TemplateAnimation.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+namespace TextAnimation
+{
+
+    [CreateAssetMenu(fileName = nameof(TemplateAnimation), menuName = "TextAnimation/Unfiltered/" + nameof(TemplateAnimation), order = 1)]
+    internal class TemplateAnimation : BaseAnimationObject
+    {
+        #region Internal
+        /// <summary>
+        /// The method to override, when creating your own implementation.
+        /// </summary>
+        /// <param name="mesh">The mesh that will be given to modify.</param>
+        /// <returns>The modified mesh.</returns>
+        internal override Mesh DoEffect(Mesh mesh)
+        {
+            return mesh; // Currently, does
+        }
+
+        #endregion
+
+        #region Private
+
+        #endregion
+    }
+}
