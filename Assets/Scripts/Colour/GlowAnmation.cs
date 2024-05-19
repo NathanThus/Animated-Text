f26099a03de14f52771d71424721604165d69789
NathanThus <nathan.thus@outlook.com>
2024-05-19 13:46:02 +0200

    🍱 Create Glow Animations

---
+++ Assets/Scripts/Colour/GlowAnmation.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace TextAnimation.Common
+{
+
+    [CreateAssetMenu(fileName = nameof(GlowColourAnimation), menuName = "TextAnimation/Colour/" + nameof(GlowColourAnimation), order = 1)]
+    internal class GlowColourAnimation : BaseAnimationObject
+    {
+        #region Internal
+        /// <summary>
+        /// The method to override, when creating your own implementation.
+        /// </summary>
+        /// <param name="mesh">The mesh that will be given to modify.</param>
+        /// <returns>The modified mesh.</returns>
+        public override Mesh DoEffect(Mesh mesh)
+        {
+            Color[] colors = mesh.colors;
+            Array.Fill<Color>(colors, Gradient.Evaluate(Mathf.Repeat(Time.time, 1f)));
+            mesh.colors = colors;
+            return mesh;
+        }
+
+        #endregion
+
+        #region Private
+
+        #endregion
+    }
+}
